2ceabc7d0b705f97359de82b3cd65a6f2fb5b096
Mateus Oliveira Patricio <o.mateus.p@gmail.com>
2021-05-12 23:19:15 -0300

    Refactored profiles urls

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProfileList
+from .views import ProfileList, ProfileCreate, ProfilePhotoList
 
 app_name = 'events'
 
 urlpatterns = [
-    path('', ProfileList.as_view(), name='profilelistcreate')
-
+    path('', ProfileList.as_view(), name='profilelist'),
+    path('create/', ProfileCreate.as_view(), name='profilecreate'),
+    path('photos/', ProfilePhotoList.as_view(), name='profilephotolist')
 ]
